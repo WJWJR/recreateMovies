06a1b632822816817d6b7e9eefabfcdc281b24ab
William J Weekes Jr <WilliamWeekes@Williams-MacBook-Pro.local>
2017-05-15 18:24:36 -0400

    api showing data in handlebar and looks similar to IMDB

---
+++ api_key.js
@@ -0,0 +1,1 @@
+var key = "e3a4e87cce4d824ba3862fe770ba2817";
--- main.js
+++ main.js
@@ -1,26 +1,30 @@
-function createMovieListing () {
-  let title = document.querySelector('.movieTitle');
-     title.textContent = json.title;
-  let poster_path = document.querySelector('img');
-     poster_path.src = 'https://image.tmdb.org/t/p/w500' + json.poster_path;
-  let overview = document.querySelector('.description');
-    overview.textContent = json.overview;
-  let release_date = document.querySelector('.openingDate');
-    release_date.textContent = json.release_date;
-  let popularity = document.querySelector('.following');
-    popularity.textContent = json.popularity;
-  let revenue = document.querySelector('.ticketSales');
-    revenue.textContent = json.revenue;
-  let runtime = document.querySelector('.duration');
-    runtime.textContent = json.runtime;
 
-  fetch("https://api.themoviedb.org/3/movie/now_playing?api_key=" + key)
-    .then(res => res.json())
-    .then(obj => obj.results)
-    .then(results => console.log(results));
 
-}
+fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=${key}`)
+  .then(function(response){
+    return response.json();
+  })
+  .then(createMovieListing);
 
+
+  function createMovieListing (data) {
+    console.log(data)
+    var rawTemplate = document.getElementById("movieTemplate").innerHTML;
+    var compiledTemplate = Handlebars.compile(rawTemplate);
+    var ourGeneratedHTML = compiledTemplate ({results: data.results});
+console.log(rawTemplate);
+    var movieContainer = document.getElementById('movie-handlebars-demo');
+    movieContainer.innerHTML = ourGeneratedHTML;
+
+  }
+
+
+
+  // fetch(`https://api.themoviedb.org/3/movie/search/company?api_key=${key}`)
+  //   .then(function(response){
+  //     return response.json();
+  //   })
+  //   .then(createMovieListing);
 
 
 
@@ -28,7 +32,7 @@ function createMovieListing () {
 // var source   = document.querySelector("#github-user-template").innerHTML;
 // var template = Handlebars.compile(source);
 // let githubPromises = classGitHub.map(user => {
-//   return fetch(`https://api.github.com/users/${user}`)
+//   return fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=${key}`)
 //    .then(object => object.json())
 // })
 // Promise.all(githubPromises).then(placeInTemplate);
@@ -41,3 +45,26 @@ function createMovieListing () {
 //   destination.innerHTML = html;
 //}
 // the html array will get joined by commas, unless you have the call to .join('') at the end
+
+
+/////////////////////////////
+
+
+// let title = document.querySelector('.movieTitle');
+//    title.textContent = json.title;
+// let poster_path = document.querySelector('img');
+//    poster_path.src = 'https://image.tmdb.org/t/p/w500' + json.poster_path;
+// let overview = document.querySelector('.description');
+//   overview.textContent = json.overview;
+// let release_date = document.querySelector('.openingDate');
+//   release_date.textContent = json.release_date;
+// let popularity = document.querySelector('.following');
+//   popularity.textContent = json.popularity;
+// let revenue = document.querySelector('.ticketSales');
+//   revenue.textContent = json.revenue;
+// let runtime = document.querySelector('.duration');
+//   runtime.textContent = json.runtime;
+
+// fetch("https://api.themoviedb.org/3/movie/now_playing?api_key=" + key + " ")
+//   .then(response => response.json())
+//   .then(createMovieListing);
